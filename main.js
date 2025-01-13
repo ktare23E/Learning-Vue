09058484dfc0492d2a1825ae5424248821094e08
Kristian <kristiankharl.tare@nmsc.edu.ph>
2025-01-13 12:45:44 +0800

    Learning about class binding and styling

--- main.js
+++ main.js
@@ -40,13 +40,47 @@
 
 
 // event listener
+// const app = Vue.createApp({
+//   data() {
+//       return {
+//           cart: 0,
+//           product: 'Socks',
+//           image: './assets/images/socks_blue.jpg',
+//           inStock: true,
+//           details: ['50% cotton', '30% wool', '20% polyester'],
+//           variants: [
+//             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+//             { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+//           ]
+//       }
+//   },
+//   methods: {
+//       addToCart() {
+//           this.cart += 1
+//       },
+//       // solution
+//       removeFromCart() {
+//           if (this.cart >= 1) {
+//               this.cart -= 1
+//           }
+//       },
+//       // solution
+//       updateImage(variantImage) {
+//           this.image = variantImage
+//       }
+//   }
+// })
+
+
+//class and styles
 const app = Vue.createApp({
   data() {
       return {
-          cart: 0,
+          cart:0,
           product: 'Socks',
+          brand: 'Vue Mastery',
           image: './assets/images/socks_blue.jpg',
-          inStock: true,
+          inStock: false,
           details: ['50% cotton', '30% wool', '20% polyester'],
           variants: [
             { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -58,13 +92,6 @@ const app = Vue.createApp({
       addToCart() {
           this.cart += 1
       },
-      // solution
-      removeFromCart() {
-          if (this.cart >= 1) {
-              this.cart -= 1
-          }
-      },
-      // solution
       updateImage(variantImage) {
           this.image = variantImage
       }
